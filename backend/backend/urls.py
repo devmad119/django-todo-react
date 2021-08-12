3885101d520ca4e951585f2c94591966d88b7c5d
devilhacker0119 <devilhacker0119@gmail.com>
2021-08-12 06:24:22 -0700

    django + react sample project

---
+++ backend/backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
---
+++ backend/todo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.db import models
+from .models import Todo
+
+# Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+
+admin.site.register(Todo, TodoAdmin)
